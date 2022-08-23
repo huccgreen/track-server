5345825c6fe136ff725cf61db47b308bdeb16d48
Sifiso Mthembu <mthembus@wavesoft.co.za>
2022-08-23 15:15:15 +0200

    first comit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.get('/' , (req , res)=>{
+    res.send("Hi there");
+});
+
+const mongoUri = "mongodb+srv://admin:admin@cluster0.nqal2.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoUri );
+
+
+mongoose.connection.on("connected" , ()=>{console.log("Connected to mongo instance")})
+mongoose.connection.on("error" , (err)=>{console.log("Error connecting ", err)} );
+
+app.listen(3000 , ()=>{
+    console.log("Listening on port  3000")
+});
